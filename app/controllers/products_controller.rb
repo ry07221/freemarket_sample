6f0288d35085e4e173f36fa5301ab6d62e68bad8
ry <zijoyrb@gamil.com>
2019-11-19 23:46:10 +0900

    1st

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,12 +6,16 @@ class ProductsController < ApplicationController
   
   def new
     @product = Product.new
+    @product.images.build  ## 新規画像用
+    # 関連したモデルのインスタンスを新しく作る場合はbuildメソッドを使用
   end
   
   def create
   end
   
   def edit
+    @product = current_user.products.find(params[:id])
+    @product.images.build  ## 新規画像用
   end
   
   def update
--- db/migrate/20190315090056_create_products.rb
+++ db/migrate/20190315090056_create_products.rb
@@ -1,10 +1,12 @@
 class CreateProducts < ActiveRecord::Migration[5.0]
+
   def change
+
     create_table :products do |t|
       t.string :name,                index: true, null: false
-      t.text :detail
+      t.text :detail,                null:false
       t.integer :shipping_fee
-      t.integer :expected_date
+      t.integer :expected_date,      null:false
       t.integer :price,              index: true, null: false
       t.integer :like_count,         default: 0
       t.string :status,              null: false
@@ -12,7 +14,11 @@ class CreateProducts < ActiveRecord::Migration[5.0]
       t.integer :transaction_status, null: false, default: 0
       t.integer :purchaser_id
       t.references :user,            null: false, foreign_key: true
+      t.references :category, null:false,forign_key: true
+      t.integer    :delivery_agency, null:false    ## 配送元地域
+
       t.timestamps
-    end
+      
   end
+
 end
